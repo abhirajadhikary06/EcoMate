60c9e9b9f59ecdd4e0fd775f6f0578dada72083c
Abhiraj Adhikary <abhirajadhikary06@gmail.com>
2025-02-27 22:09:15 +0530

    Added no changes

--- ecofix/climate_tracker/admin.py
+++ ecofix/climate_tracker/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import ShopItem
 
-class ShopItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'points_required')
-    readonly_fields = ('name', 'description', 'points_required')  # Make fields read-only
-
-admin.site.register(ShopItem, ShopItemAdmin)
+# Register your models here.
